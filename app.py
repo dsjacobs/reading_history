85d4e9477a109ace483d68bb7b4bae4ae0ead08f
Danielle Jacobs <47550694+dsjacobs@users.noreply.github.com>
2025-05-05 21:57:57 -0400

    add load dot env

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, g
 import os
 import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 DATABASE = 'database.db'
